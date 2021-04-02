ea6c05c8ea329439cc01d477b8650a63b28eff7c
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-02 15:38:32 +0530

    bootstrap.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ export default function App() {
 function ComponentA() {
   return (
     <div>
-      <div className="text-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam eum ipsam natus, mollitia unde, repellendus minus quaerat nostrum eos aliquam aliquid sunt facilis officia eveniet laudantium, ea adipisci distinctio quo.</div>
-      <button className="btn btn-primary">Click Me</button>
+      <div className="bg-danger text-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam eum ipsam natus, mollitia unde, repellendus minus quaerat nostrum eos aliquam aliquid sunt facilis officia eveniet laudantium, ea adipisci distinctio quo.</div>
+      <button className="btn btn-warning">Click Me</button>
     </div>
   );
 }

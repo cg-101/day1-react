ba94dac76522e674ea7be687915bc02e3b9b8fcd
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-02 12:54:10 +0530

    working with map / loop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,39 @@ export default function App() {
     <div>
       <ComponentA />
       <ComponentA />
-      <ComponentA />
+      <ComponentB />
+    </div>
+  );
+}
+
+/**
+ * <ComponentB />
+ * @returns
+ */
+function ComponentB() {
+  let i = 100;
+  let list = [1, 2, 3];
+
+  return (
+    <div>
+      <div>Lorem, ipsum. {i}</div>
+
+      {list.map(function () {
+        return <div>AAA</div>;
+      })}
+
+      {list.map(() => {
+        return <div>BBBB</div>;
+      })}
+
+      
+      {list.map(() => <div>DDD</div>)}  
+
+      {list.map(() => (
+        <div>
+          <div>CCC</div>
+        </div>
+      ))}
     </div>
   );
 }

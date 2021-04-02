9c0d64929a43cdfab81b268b1d6b86f6785e7025
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-02 15:26:05 +0530

    working with bootstrap.

--- src/App.js
+++ src/App.js
@@ -9,17 +9,10 @@ export default function App() {
 }
 
 function ComponentA() {
-  let [counter, setCounter] = useState(100);
-  
-  const icounter = () => {
-    console.log(counter);
-    setCounter(counter + 1);
-  }
-  
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={icounter}>Update Counter</button>
+      <div className="text-info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam eum ipsam natus, mollitia unde, repellendus minus quaerat nostrum eos aliquam aliquid sunt facilis officia eveniet laudantium, ea adipisci distinctio quo.</div>
+      <button className="btn btn-primary">Click Me</button>
     </div>
   );
 }

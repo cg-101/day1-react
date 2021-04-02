c6d077fe17fa34f9fbf550de68b2691fe246a301
Santosh Mondal <santosh.ece06@gmail.com>
2021-04-02 12:32:41 +0530

    reuability

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-
+import React from "react";
 
 export default function App() {
-  
-  return <div>Hello Worldddd</div>
+  return (
+    <div>
+      <ComponentA />
+      <ComponentA />
+      <ComponentA />
+      <ComponentA />
+      <ComponentA />
+    </div>
+  );
 }
 
-
+/**
+ * <ComponentA />
+ * @returns
+ */
+function ComponentA() {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Laboriosam ipsum repudiandae vel, qui nihil ipsa commodi repellat
+        eaque tempora ducimus veniam cupiditate maxime! Ducimus accusantium
+        perspiciatis laboriosam aut quae neque!
+      </div>
+      <hr />
+    </div>
+  );
+}
